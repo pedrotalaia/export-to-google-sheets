dea291339323f4dc54b91dfc3823b8441ade3ab3
pedrotalaia <josepedrotalaia@gmail.com>
2024-07-07 08:45:32 +0100

    app js new changes to card badges

--- js/app.js
+++ js/app.js
@@ -1,29 +1,17 @@
+console.log('app.js running');
 
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
-      let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-      
-      var getCardName = t.card("name").get("name")
-      return [
-        {
-            text: getCardName,
-            color: "pink"
-        },
-        /* DYNAMIC FUNCTION TO TEST NEXT 
-        {
-        dynamic: function () {
-            // we could also return a Promise that resolves to
-            // this as well if we needed to do something async first
-            return {
-              text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
-              icon: "./images/icon.svg",
-              color: "green",
-              refresh: 10, // in seconds
-            };
-          }
-        } */
-      ]
-      
+        console.log('card badges initialization is done!');
+        return t.card('all')
+        .then((cards) => {
+            console.log(cards);
+            return [
+                {
+                    text: getCardName,
+                    color: "pink"
+                }];
+        });
     },
   });
   
